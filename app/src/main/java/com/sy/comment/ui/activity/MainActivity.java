48b9cafe079ff773cd7c01a157a35801eaa19642
HassGy <1191600519@qq.com>
2020-05-07 15:22:48 +0800

    修改

---
+++ app/src/main/java/com/sy/comment/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sy.comment.ui.activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sy.comment.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
